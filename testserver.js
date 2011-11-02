5a94758c5473f833b8b7d884db26a5490f13a46c
Joe Markwardt <jmarkwar@gmail.com>
2011-11-02 16:44:53 +0000

    moving stuff away from c9.io

--- testserver.js
+++ testserver.js
@@ -1,12 +1,25 @@
-var express = require('express');
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
 
-var app = express.createServer(express.logger());
+app.listen(80);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
