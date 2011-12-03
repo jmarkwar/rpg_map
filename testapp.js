19040a5de2d9f1d366bc1c6d094082dda67915a6
Joe Markwardt <jmarkwar@gmail.com>
2011-12-03 21:38:58 +0000

    trying to get redis working

--- testapp.js
+++ testapp.js
@@ -1,14 +1,12 @@
-var app = require('express').createServer(),
-    io = require('socket.io').listen(app);
-
+//setup express
+var express = require('express');
 var port = process.env.PORT || 80;
+var app = express.createServer();
+app.use(express.static(__dirname + "/Client"));
 
-app.listen(port);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
+//setup socket.io
+var io = require('socket.io').listen(app);
 io.sockets.on('connection', function (socket) {
   socket.emit('Welcome', 'Welcome!'); 
   socket.on('NewPlayer', function (player) {
@@ -19,4 +17,20 @@ io.sockets.on('connection', function (socket) {
       console.log(player);
       socket.broadcast.emit('PlayerLeft',player);
   });
+});
+
+//setup redis
+var redis = require('redis-url');
+var db = redis.connect(process.env.REDISTOGO_URL);
+
+
+
+//start the app
+app.listen(port);
+console.log("app is listening on %d", port);
+
+db.set('foo', 'bar');
+db.get('foo', function(err, value) {
+  console.log('foo is: ' + value);
+  console.log(err);
 });
